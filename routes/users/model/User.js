7b9917eb19d88e4457029f1ca5c819e7102e9985
Jonathan Drew <jonathan.drew@codeimmersives.com>
2020-06-10 12:27:27 -0400

    finish copying

--- routes/users/model/User.js
+++ routes/users/model/User.js
@@ -1,35 +1,29 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const moment = require("moment");
 const now = moment();
-
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   username: {
     type: String,
     trim: true,
-    required: "username is required",
-    unique: "Username already exists, please choose another one",
+    require: "Username is required",
+    unique: "Username already exists",
   },
   email: {
     type: String,
     trim: true,
     unique: "Email already exists",
-    match: [/.+\@..+/, "Please fill a valid email address"],
+    match: [/.+\@..+/, "Please fill in valid email address"],
     required: "Email is required",
   },
   password: {
     type: String,
-    required: "password is required",
+    required: "Password is required",
   },
   userCreated: {
     type: String,
-    default: now.format("dddd, MMMM Do YYYY, h:mm:ss a"),
+    default: now.format("dddd, MMMM, Do YYYY, h:mm:ss a"),
   },
-  expenses: [{ type: mongoose.Schema.ObjectId, ref: "Expense" }],
-  myDrinks: [
-    {
-      drinks: String,
-    },
-  ],
+  drinks: [{ type: mongoose.Schema.ObjectId, ref: "Drinks" }],
 });
-
 module.exports = mongoose.model("User", UserSchema);
